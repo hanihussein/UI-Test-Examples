3974a763dddeb2dfcabf8d95db28610c5cd92ece
hanizaki <hani.hussien.cs@gmail.com>
2020-12-09 17:33:11 +0100

    - Create Main Activity - Create Secondary Activity

--- app/src/androidTest/java/com/hani/uitestbasics/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/hani/uitestbasics/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.hani.uitestingbasics
+package com.hani.uitestbasics
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
--- app/src/test/java/com/hani/uitestbasics/ExampleUnitTest.kt
+++ app/src/test/java/com/hani/uitestbasics/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.hani.uitestingbasics
+package com.hani.uitestbasics
 
 import org.junit.Test
 
