d02eb109b6c9548b5801153627aa8bbb624dd04a
hanizaki <hani.hussien.cs@gmail.com>
2020-12-15 18:06:00 +0100

    - Testing Dialogs

--- app/src/main/java/com/hani/uitestbasics/main/ui/MainActivity.kt
+++ app/src/main/java/com/hani/uitestbasics/main/ui/MainActivity.kt
@@ -1,57 +1,43 @@
 package com.hani.uitestbasics.main.ui
 
-import android.app.Activity
-import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.input.input
 import com.hani.uitestbasics.R
 import kotlinx.android.synthetic.main.activity_main.*
 
-const val CAMERA_REQUEST_CODE = 1234
-const val KEY_IMAGE_DATA = "data"
-
 class MainActivity : AppCompatActivity() {
-
-    private val TAG: String = "AppDebug"
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button_camera.setOnClickListener {
-            pickFromGallery()
+        button_launch_dialog.setOnClickListener {
+            showDialog()
         }
-
     }
 
+    private fun showDialog() {
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK) {
-            Log.d(TAG, "RESULT_OK")
-            when (requestCode) {
+        MaterialDialog(this).show {
 
-                CAMERA_REQUEST_CODE -> {
-                    Log.d(TAG, "GALLERY_REQUEST_CODE detected.")
-                    data?.extras?.let { extras ->
-                        if (!extras.containsKey(KEY_IMAGE_DATA)) {
-                            return
-                        }
-                        val imageBitmap = extras[KEY_IMAGE_DATA] as Bitmap?
-                        image.setImageBitmap(imageBitmap)
-                    }
-                }
+            input(
+                waitForPositiveButton = true,
+                allowEmpty = false
+            ) { dialog, name ->
+                setNameToTextView(name.toString())
             }
+            title(R.string.text_enter_name)
+            positiveButton(R.string.text_ok)
+
         }
     }
 
-    private fun pickFromGallery() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(intent, CAMERA_REQUEST_CODE)
+
+    private fun setNameToTextView(name: String) {
+        text_name.text = name
     }
 
 }
